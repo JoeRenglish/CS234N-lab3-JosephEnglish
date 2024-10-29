a433aedd6e8f7103c110c522dcefcf40aa21184f
Joseph English <work@Josephs-MacBook-Pro.local>
2024-10-29 11:57:37 -0700

    Added CustomerProps. Added CustomerPropsTests. Added CustomerDB.

---
+++ MMABooksFramework2022/MMABooksTests/CustomerPropsTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using MMABooksProps;
+using System;
+
+namespace MMABooksTests
+{
+	[TestFixture]
+	public class CustomerPropsTests
+	{
+		CustomerProps props;
+		[SetUp]
+        public void Setup()
+        {
+            props = new CustomerProps();
+            props.CustomerId = 1;
+            props.Name = "Harry Hole";
+            props.Address = "555 London Drive";
+            props.City = "Bath";
+            props.State = "Mississippi";
+            props.ZipCode = "57471";
+        }
+
+        [Test]
+        public void TestGetState()
+        {
+            string jsonString = props.GetState();
+            Console.WriteLine(jsonString);
+            Assert.IsTrue(jsonString.Contains(props.CustomerId.ToString()));
+            Assert.IsTrue(jsonString.Contains(props.Name));
+            Assert.IsTrue(jsonString.Contains(props.Address));
+            Assert.IsTrue(jsonString.Contains(props.City));
+            Assert.IsTrue(jsonString.Contains(props.State));
+            Assert.IsTrue(jsonString.Contains(props.ZipCode));
+        }
+
+        [Test]
+        public void TestSetState()
+        {
+            string jsonString = props.GetState();
+            CustomerProps newProps = new CustomerProps();
+            newProps.SetState(jsonString);
+            Assert.AreEqual(props.CustomerId, newProps.CustomerId);
+            Assert.AreEqual(props.Name, newProps.Name);
+            Assert.AreEqual(props.Address, newProps.Address);
+            Assert.AreEqual(props.City, newProps.City);
+            Assert.AreEqual(props.State, newProps.State);
+            Assert.AreEqual(props.ZipCode, newProps.ZipCode);
+            Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+        }
+
+        [Test]
+        public void TestClone()
+        {
+            CustomerProps newProps = (CustomerProps)props.Clone();
+            Assert.AreEqual(props.CustomerId, newProps.CustomerId);
+            Assert.AreEqual(props.Name, newProps.Name);
+            Assert.AreEqual(props.Address, newProps.Address);
+            Assert.AreEqual(props.City, newProps.City);
+            Assert.AreEqual(props.State, newProps.State);
+            Assert.AreEqual(props.ZipCode, newProps.ZipCode);
+            Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+        }
+    }
+}
+
